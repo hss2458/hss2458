225c33ddd385638c61062dff3ef35463e055f315
micomihye <mihye458@gmail.com>
2020-11-19 15:35:30 +0900

    first commit

---
+++ js/lib/script.js
@@ -0,0 +1,8 @@
+// var list = document.getElementsByClassName('contents-list');
+// var title = document.getElementsByClassName('title');
+
+// list[0].onmouseover = function() {
+//     console.log('test');
+//     title[0].style.opacity = "0";
+//     // box.style.width = "1000px";
+// }
